1b85a69b4c5d08784acde558d858b451aa6241fc
AugustinChp <augustinchplt@gmail.com>
2021-11-22 21:51:18 +0100

    Add requirements file

--- mycellwine/settings.py
+++ mycellwine/settings.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 # standard library
 import os
 from pathlib import Path
-import environ
+from dotenv import load_dotenv
 
 # third-party
 
@@ -24,14 +24,13 @@ import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-env = environ.Env()
-environ.Env.read_env()
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
