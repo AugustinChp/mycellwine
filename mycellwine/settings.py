be9ea1257a7e37da3e5c355a13b1bb1fd7a28d8b
AugustinChp <augustinchplt@gmail.com>
2021-11-22 22:19:30 +0100

    Set environment conditions

--- mycellwine/settings.py
+++ mycellwine/settings.py
@@ -30,7 +30,12 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+if os.environ.get('ENV') == 'PRODUCTION':
+    DEBUG = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
+else:
+    SECRET_KEY = "****1234****"
+    DEBUG = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
