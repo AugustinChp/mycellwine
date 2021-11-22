438718789b180b08a8aad0e144d2a2e266686b1f
AugustinChp <augustinchplt@gmail.com>
2021-11-22 21:43:58 +0100

    define variable environment

--- mycellwine/settings.py
+++ mycellwine/settings.py
@@ -9,18 +9,29 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+# future
 
+# standard library
+import os
 from pathlib import Path
+import environ
+
+# third-party
+
+# Django
+
+# local Django
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vj+-253l5l07wg)dgpbpfzk1#7nf=6jjf!#7&k6jm$5&54^wzg'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
