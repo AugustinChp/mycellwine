e473aaf52e5f657d6a230607ac0046f637c1662c
AugustinChp <augustinchplt@gmail.com>
2021-12-17 00:03:12 +0100

    setup heroku app

--- mycellwine/settings.py
+++ mycellwine/settings.py
@@ -159,7 +159,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 if os.environ.get('ENV') == 'PRODUCTION':
 
